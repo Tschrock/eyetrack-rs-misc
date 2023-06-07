a0700d37181bc0f00ac3151fe567f04eaad1439a
Tyler Schrock <tschrock123@gmail.com>
2023-06-06 21:24:11 -0400

    Initial Commit

---
+++ src/image_processing.rs
@@ -0,0 +1,162 @@
+//! Image processing module
+//!
+//! This module contains the image processing functions.
+
+use opencv::core::{Mat, Point, Point2f, Rect, Scalar, Size, Vector};
+use opencv::prelude::*;
+
+pub fn crop_rotate(
+    image: &Mat,
+    x: i32,
+    y: i32,
+    height: i32,
+    width: i32,
+    angle: f64,
+) -> Result<Mat, opencv::Error> {
+    let rect = Rect::new(x, y, width, height);
+    let cropped = Mat::roi(&image, rect)?;
+    let size = cropped.size()?;
+    let center = Point2f::new(width as f32 / 2.0, height as f32 / 2.0);
+    let rotation_matrix = opencv::imgproc::get_rotation_matrix_2d(center, angle, 1.0)?;
+
+    let mut rotated = Mat::default();
+    opencv::imgproc::warp_affine(
+        &cropped,
+        &mut rotated,
+        &rotation_matrix,
+        size,
+        opencv::imgproc::INTER_LINEAR,
+        opencv::core::BORDER_CONSTANT,
+        Scalar::all(255.0),
+    )?;
+
+    Ok(rotated)
+}
+
+pub fn greyscale(image: &Mat) -> Result<Mat, opencv::Error> {
+    let mut grey = Mat::default();
+    opencv::imgproc::cvt_color(&image, &mut grey, opencv::imgproc::COLOR_BGR2GRAY, 0)?;
+    Ok(grey)
+}
+
+pub fn circular_mask(image: &Mat, cx: i32, cy: i32, radius: i32) -> Result<Mat, opencv::Error> {
+    let size = image.size()?;
+    let mut mask = Mat::new_size_with_default(size, opencv::core::CV_8UC1, Scalar::all(0.0))?;
+
+    opencv::imgproc::circle(
+        &mut mask,
+        Point::new(cx, cy),
+        radius,
+        Scalar::all(255.0),
+        opencv::imgproc::FILLED,
+        opencv::imgproc::LINE_8,
+        0,
+    )?;
+
+    let mut masked_img = Mat::default();
+    opencv::core::bitwise_and(image, image, &mut masked_img, &mask)?;
+
+    let white = Mat::new_size_with_default(size, opencv::core::CV_8UC1, Scalar::all(255.0))?;
+
+    let mask_inv = opencv::core::sub_scalar_mat(Scalar::all(255.0), &mask)?;
+
+    let mut masked_white = Mat::default();
+    opencv::core::bitwise_and(&white, &white, &mut masked_white, &mask_inv)?;
+
+    let mut output = Mat::default();
+    opencv::core::add(
+        &masked_img,
+        &masked_white,
+        &mut output,
+        &opencv::core::no_array(),
+        -1,
+    )?;
+
+    Ok(output)
+}
+
+pub fn threshold(image: &Mat, threshold: f64) -> Result<Mat, opencv::Error> {
+    let mut output = Mat::default();
+    opencv::imgproc::threshold(
+        &image,
+        &mut output,
+        threshold,
+        255.0,
+        opencv::imgproc::THRESH_BINARY,
+    )?;
+    Ok(output)
+}
+
+pub fn morphology(
+    image: &Mat,
+    kernel_width: i32,
+    kernel_height: i32,
+) -> Result<Mat, opencv::Error> {
+    let kernel = opencv::imgproc::get_structuring_element(
+        opencv::imgproc::MORPH_ELLIPSE,
+        Size::new(kernel_width, kernel_height),
+        Point::new(-1, -1),
+    )?;
+
+    let mut opening = Mat::default();
+    opencv::imgproc::morphology_ex(
+        &image,
+        &mut opening,
+        opencv::imgproc::MORPH_OPEN,
+        &kernel,
+        Point::new(-1, -1),
+        1,
+        opencv::core::BORDER_CONSTANT,
+        opencv::imgproc::morphology_default_border_value()?,
+    )?;
+
+    let mut closing = Mat::default();
+    opencv::imgproc::morphology_ex(
+        &opening,
+        &mut closing,
+        opencv::imgproc::MORPH_CLOSE,
+        &kernel,
+        Point::new(-1, -1),
+        1,
+        opencv::core::BORDER_CONSTANT,
+        opencv::imgproc::morphology_default_border_value()?,
+    )?;
+
+    opencv::core::sub_scalar_mat(Scalar::all(255.0), &closing)?.to_mat()
+}
+
+pub fn find_contours(image: &Mat) -> Result<Vector<Mat>, opencv::Error> {
+    let mut contours = opencv::types::VectorOfMat::new();
+    opencv::imgproc::find_contours(
+        &image,
+        &mut contours,
+        opencv::imgproc::RETR_TREE,
+        opencv::imgproc::CHAIN_APPROX_NONE,
+        Point::new(0, 0),
+    )?;
+    Ok(contours)
+}
+
+pub fn contours_to_convex_hulls(contours: &Vector<Mat>) -> Result<Vector<Vector<Point>>, opencv::Error> {
+    let mut convex_hulls = opencv::types::VectorOfVectorOfPoint::new();
+    for contour in contours {
+        let mut hull = opencv::types::VectorOfPoint::new();
+        opencv::imgproc::convex_hull(&contour, &mut hull, false, false)?;
+        convex_hulls.push(hull);
+    }
+    Ok(convex_hulls)
+}
+
+pub fn find_largest_hull(hulls: &Vector<Vector<Point>>) -> Result<Option<Vector<Point>>, opencv::Error> {
+    let mut largest_hull = None;
+    let mut largest_hull_area = 0.0;
+    for hull in hulls {
+        // println!("hull: {:?}", hull);
+        let area = opencv::imgproc::contour_area(&hull, false)?;
+        if area > largest_hull_area {
+            largest_hull = Some(hull);
+            largest_hull_area = area;
+        }
+    }
+    Ok(largest_hull)
+}
---
+++ src/ransac.rs
@@ -0,0 +1,114 @@
+use nalgebra::MatrixXx2;
+use rand::prelude::*;
+use rand::Rng;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum RansacError {
+    #[error("Not enough data points to fit an ellipse")]
+    NotEnoughDataPoints,
+    #[error("Failed to find a good fit")]
+    FailedToFindGoodFit,
+    #[error("Failed to invert matrix")]
+    InversionFailed,
+}
+
+pub fn fit_rotated_ellipse_ransac<R>(
+    data: &MatrixXx2<f64>,
+    rng: &mut R,
+    max_iterations: Option<usize>,
+    samples_to_fit: Option<usize>,
+    error_threshold: Option<f64>,
+) -> Result<(f64, f64, f64, f64, f64), RansacError>
+where
+    R: Rng + ?Sized,
+{
+    let max_iterations = max_iterations.unwrap_or(5);
+    let samples_to_fit = samples_to_fit.unwrap_or(10);
+    let error_threshold = error_threshold.unwrap_or(80.0);
+
+    let mut count_max = 0;
+    let mut effective_sample: Option<MatrixXx2<f64>> = None;
+
+    if data.nrows() < samples_to_fit {
+        return Err(RansacError::NotEnoughDataPoints);
+    }
+
+    for _ in 0..max_iterations {
+        let sample = MatrixXx2::from_rows(&data.row_iter().choose_multiple(rng, samples_to_fit));
+        let (a, b, c, d, e, f) = rotated_ellipse_data(&sample)?;
+
+        let ellipse_model =
+            |x: f64, y: f64| (a * x.powi(2) + b * x * y + c * y.powi(2) + d * x + e * y + f);
+
+        let rows = &data
+        .row_iter()
+        .filter(|row| ellipse_model(row[0], row[1]).abs() < error_threshold)
+        .collect::<Vec<_>>();
+
+        if rows.len() > count_max {
+            count_max = rows.len();
+            effective_sample = Some(MatrixXx2::from_rows(rows));
+        }
+    }
+
+    match effective_sample {
+        None => return Err(RansacError::FailedToFindGoodFit),
+        Some(s) => {
+            fit_rotated_ellipse(&s)
+        }
+    }
+}
+
+pub fn fit_rotated_ellipse(
+    data: &MatrixXx2<f64>,
+) -> Result<(f64, f64, f64, f64, f64), RansacError> {
+    let (a, b, c, d, e, f) = rotated_ellipse_data(data)?;
+
+    let theta = 0.5 * (b / (a - c).atan());
+
+    let cx = (2.0 * c * d - b * e) / (b.powi(2) - 4.0 * a * c);
+    let cy = (2.0 * a * e - b * d) / (b.powi(2) - 4.0 * a * c);
+
+    let cu = a * cx.powi(2) + b * cx * cy + c * cy.powi(2) - f;
+
+    let w = (cu
+        / (a * theta.cos().powi(2) + b * theta.cos() * theta.sin() + c * theta.sin().powi(2)))
+    .sqrt();
+    let h = (cu
+        / (a * theta.sin().powi(2) - b * theta.cos() * theta.sin() + c * theta.cos().powi(2)))
+    .sqrt();
+
+    Ok((cx, cy, w, h, theta))
+}
+
+fn rotated_ellipse_data(
+    data: &nalgebra::MatrixXx2<f64>,
+) -> Result<(f64, f64, f64, f64, f64, f64), RansacError> {
+    let xs = data.column(0);
+    let ys = data.column(1);
+
+    let j = nalgebra::MatrixXx5::from_row_iterator(
+        data.nrows(),
+        xs.iter()
+            .zip(ys.iter())
+            .flat_map(|(x, y)| [x * y, y * y, *x, *y, 1.0]),
+    );
+
+    let y = xs.map(|v| -1.0 * v * v);
+
+    let p = (j.transpose() * &j)
+        .try_inverse()
+        .ok_or(RansacError::InversionFailed)?
+        * j.transpose()
+        * &y;
+
+    let a = 1.0;
+    let b = p[(0, 0)];
+    let c = p[(1, 0)];
+    let d = p[(2, 0)];
+    let e = p[(3, 0)];
+    let f = p[(4, 0)];
+
+    Ok((a, b, c, d, e, f))
+}
